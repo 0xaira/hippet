d0c53c171d784e8a5ab5c497818e76fb11793fb6
Aira Jena <airajena.hack@gmail.com>
2024-07-02 21:01:59 +0530

    feat: Add e-commerce functionality to product pages
    - Added `Page` component for product details with breadcrumbs, product info, and image slider.
    - Integrated `AddToCartButton` for adding products to the cart and `ProductReel` for similar products.
    - Created `ProductsPage` for displaying product reels with sorting and filtering.
    - Developed `AddToCartButton` with state management for success feedback.
    - Implemented `Cart` component for viewing, removing, and checking out cart items.
    - Added `CartItem` component to render individual items in the cart.
    - Set up Zustand for cart state management with actions for add/remove/clear and local storage persistence.
    - Included utility functions and configurations for price formatting and product categories.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -14,13 +14,28 @@ import { formatPrice } from '@/lib/utils'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
 import Image from 'next/image'
-
+import { useCart } from '@/hooks/use-cart'
+import { ScrollArea } from './ui/scroll-area'
+import CartItem from './CartItem'
+import { useEffect, useState } from 'react'
 
 const Cart = () => {
+  const { items } = useCart()
+  const itemCount = items.length
+
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  const cartTotal = items.reduce(
+    (total, { product }) => total + product.price,
+    0
+  )
 
   const fee = 1
-  const itemCount = 0
-  const cartTotal = 0
+
   return (
     <Sheet>
       <SheetTrigger className='group -m-2 flex items-center p-2'>
@@ -29,6 +44,7 @@ const Cart = () => {
           className='h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
         />
         <span className='ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800'>
+          {isMounted ? itemCount : 0}
         </span>
       </SheetTrigger>
       <SheetContent className='flex w-full flex-col pr-0 sm:max-w-lg'>
@@ -37,6 +53,16 @@ const Cart = () => {
         </SheetHeader>
         {itemCount > 0 ? (
           <>
+            <div className='flex w-full flex-col pr-6'>
+              <ScrollArea>
+                {items.map(({ product }) => (
+                  <CartItem
+                    product={product}
+                    key={product.id}
+                  />
+                ))}
+              </ScrollArea>
+            </div>
             <div className='space-y-4 pr-6'>
               <Separator />
               <div className='space-y-1.5 text-sm'>
